a9b9275a59a844cccf654d5a81bd9e60639d9348
Aizat Faiz <aizat.faiz@gmail.com>
2019-02-21 11:39:04 +0800

    fix: styling

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ class App extends Component {
         <Container className="pt-3">
           <h1><code>Frequently Used</code></h1>
           <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
-          <Moment fmt="YYYY/MM/DD - [W]W/[D]E dddd" />
+          <Moment fmt="YYYY/MM/DD [W]W/[D]E dddd" />
           <Moment fmt="LTS: " />
           <h1><code>Date</code></h1>
           <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a>
