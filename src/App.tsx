428c8731b809032990d88c8b5105782ba905a7c1
Aizat Faiz <aizat.faiz@gmail.com>
2019-03-06 16:36:24 +0800

    feature: add refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import strftime from 'strftime';
 import {
   Button,
@@ -11,7 +11,11 @@ import Navbar from './Navbar';
 import moment from 'moment';
 const {CopyToClipboard} = require('react-copy-to-clipboard');
 
-const mmt = moment();
+const initialDate = new Date();
+const initialMoment = moment();
+
+const DateContext = React.createContext<Date>(new Date());
+const MomentContext = React.createContext<moment.Moment>(initialMoment);
 
 function Field(props: {content: string, code: string}) {
   return (
@@ -30,46 +34,71 @@ function Field(props: {content: string, code: string}) {
 }
 
 function Time(props: {fmt: string}) {
-  const str = strftime(props.fmt);
-  return <Field code={`strftime("${props.fmt}")`} content={str} />;
+  return (
+    <DateContext.Consumer>
+      {(date) => {
+        const str = strftime(props.fmt, date);
+        return <Field code={`strftime("${props.fmt}")`} content={str} />;
+      }}
+    </DateContext.Consumer>
+  );
 }
 
 function Moment(props: {fmt: string}) {
-  const str = mmt.format(props.fmt);
-  return <Field code={`moment().format("${props.fmt}")`} content={str} />;
+  return (
+    <MomentContext.Consumer>
+      {(mmt) => {
+        const str = mmt.format(props.fmt);
+        return <Field code={`moment().format("${props.fmt}")`} content={str} />;
+      }}
+    </MomentContext.Consumer>
+  );
 }
 
 function App() {
+  let [date, setDate] = useState(initialDate);
+  let [mmt, setMoment] = useState(initialMoment);
+
+  const onClick = () => {
+    setMoment(moment());
+    setDate(new Date());
+  }
+
   return (
     <div>
-      <Navbar />
-      <Container className="pt-3">
-        <h1><code>Frequently Used</code></h1>
-        <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
-        <Moment fmt="YYYY/MM/DD [W]W/[D]E dddd" />
-        <Moment fmt="LTS: " />
-        <h1><code>Date</code></h1>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a>
-        <Field code="Math.floor(Date.now() / 1000)" content={Math.floor(Date.now() / 1000).toString()} />
-        <Field code="Date.now()" content={Date.now().toString()} />
-        <Field code="new Date().toISOString()" content={new Date().toISOString()} />
-        <h1><code>strftime</code></h1>
-        <a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a>
-        <Time fmt="%H:%M%P" />
-        <Time fmt="%Y/%m/%d" />
-        <Time fmt="%Y/%m/%d %H:%M:%S%P" />
-        <h1><code>moment</code></h1>
-        <a href="http://momentjs.com/">http://momentjs.com/</a>
-        <Moment fmt="dddd Do MMMM YYYY LTS" />
-        <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
-        Packages:
-        <ul>
-          <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a></li>
-          <li><a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a></li>
-          <li><a href="http://momentjs.com/">http://momentjs.com/</a></li>
-          <li><a href="https://date-fns.org/">https://date-fns.org/</a></li>
-        </ul>
-      </Container>
+      <DateContext.Provider value={date}>
+        <MomentContext.Provider value={mmt}>
+          <Navbar />
+          <Container className="pt-3">
+            <Button onClick={onClick} className="float-right">Refresh</Button>
+            <h1><code>Frequently Used</code></h1>
+            <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
+            <Moment fmt="YYYY/MM/DD [W]W/[D]E dddd" />
+            <Moment fmt="LTS: " />
+            <h1><code>Date</code></h1>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a>
+            <Field code="Math.floor(Date.now() / 1000)" content={Math.floor(Date.now() / 1000).toString()} />
+            <Field code="Date.now()" content={Date.now().toString()} />
+            <Field code="new Date().toISOString()" content={new Date().toISOString()} />
+            <h1><code>strftime</code></h1>
+            <a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a>
+            <Time fmt="%H:%M%P" />
+            <Time fmt="%Y/%m/%d" />
+            <Time fmt="%Y/%m/%d %H:%M:%S%P" />
+            <h1><code>moment</code></h1>
+            <a href="http://momentjs.com/">http://momentjs.com/</a>
+            <Moment fmt="dddd Do MMMM YYYY LTS" />
+            <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
+            Packages:
+            <ul>
+              <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a></li>
+              <li><a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a></li>
+              <li><a href="http://momentjs.com/">http://momentjs.com/</a></li>
+              <li><a href="https://date-fns.org/">https://date-fns.org/</a></li>
+            </ul>
+          </Container>
+        </MomentContext.Provider>
+      </DateContext.Provider>
     </div>
   );
 }
