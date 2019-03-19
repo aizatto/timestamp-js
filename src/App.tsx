96124e2eeca2d79878a8dad3d169c52f45ef84db
Aizat Faiz <aizat.faiz@gmail.com>
2019-03-19 14:20:24 +0800

    feature: add keyboard shortcuts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 /* eslint-env browser */
-import React, { useState, useRef } from 'react';
+/* eslint-disable react/no-multi-comp */
+import React, { useState, useRef, useImperativeHandle } from 'react';
 import strftime from 'strftime';
 import {
+  Alert,
   Button,
   Container,
   Input,
@@ -9,9 +11,28 @@ import {
   InputGroupAddon,
 } from 'reactstrap';
 import moment from 'moment';
+import copyToClipboard from 'copy-to-clipboard';
 import Navbar from './Navbar';
 
-const {CopyToClipboard} = require('react-copy-to-clipboard');
+interface CopyHandler {
+  value: () => string,
+}
+
+/* eslint-disable no-unused-vars */
+enum KeyCode {
+//  DIGIT_0 = 48,
+  DIGIT_1 = 49,
+  DIGIT_9 = 57,
+  A = 65,
+  Z = 90,
+}
+
+enum KeyboardToIndex {
+  KEY_1 = 0,
+  KEY_2,
+  KEY_3,
+}
+/* eslint-enable -no-unused-vars */
 
 const history = {
   dates: [
@@ -25,49 +46,68 @@ const history = {
 const DateContext = React.createContext<Date>(history.dates[0]);
 const MomentContext = React.createContext<moment.Moment>(history.moments[0]);
 
-function Field(props: {content: string, code: string}) {
+const Field = React.forwardRef((props: {content: string, code: string}, ref) => {
+  if (ref) {
+    useImperativeHandle(
+      ref,
+      () => ({
+        value: () => {
+          return props.content;
+        }
+      }),
+      [props.content],
+    );
+  }
   return (
     <div className="mb-2">
       <code>{props.code}</code>
       <InputGroup>
         <InputGroupAddon addonType="prepend">
-          <CopyToClipboard text={props.content}>
-            <Button>Copy</Button>
-          </CopyToClipboard>
+          <Button onClick={() => copyToClipboard(props.content)}>Copy</Button>
         </InputGroupAddon>
         <Input value={props.content} readOnly />
       </InputGroup>
     </div>
   );
-}
+});
 
-function Time(props: {fmt: string}) {
+const Time = React.forwardRef((props: {fmt: string}, ref) => {
   return (
     <DateContext.Consumer>
       {(date) => {
         const str = strftime(props.fmt, date);
-        return <Field code={`strftime("${props.fmt}")`} content={str} />;
+        return <Field ref={ref} code={`strftime("${props.fmt}")`} content={str} />;
       }}
     </DateContext.Consumer>
   );
-}
+});
 
-function Moment(props: {fmt: string}) {
+const Moment = React.forwardRef((props: {fmt: string }, ref) => {
   return (
     <MomentContext.Consumer>
       {(mmt) => {
         const str = mmt.format(props.fmt);
-        return <Field code={`moment().format("${props.fmt}")`} content={str} />;
+        return <Field ref={ref} code={`moment().format("${props.fmt}")`} content={str} />;
       }}
     </MomentContext.Consumer>
   );
-}
+});
 
 
 function App() {
   const [date, setDate] = useState(history.dates[0]);
   const [mmt, setMoment] = useState(history.moments[0]);
+  const [alertText, setAlertText] = useState<JSX.Element | null>();
   const windowOnKeyDownListener = useRef<(event: KeyboardEvent) => void>();
+
+  // I don't use a loop because it is a Hook Rule to not place hooks
+  // in loops
+  // See https://reactjs.org/docs/hooks-rules.html#only-call-hooks-at-the-top-level
+  const refs = [
+    useRef({} as CopyHandler),
+    useRef({} as CopyHandler),
+    useRef({} as CopyHandler),
+  ];
 
   const refreshTimestamps = () => {
     const newDate = new Date();
@@ -76,15 +116,44 @@ function App() {
     history.moments.push(newMmt);
     setDate(newDate);
     setMoment(newMmt);
+    setAlertText(
+      <>
+        Refreshed at: 
+        {' '}
+        <code>{newMmt.format("YYYY/MM/DD LTS - [W]W/[D]E dddd")}</code>
+      </>
+    );
   }
 
   if (!windowOnKeyDownListener.current) {
-    const listener = () => {
-      refreshTimestamps();
+    const listener = (event: KeyboardEvent) => {
+      if ((KeyCode.DIGIT_1 <= event.keyCode && event.keyCode <= KeyCode.DIGIT_9)) {
+        const index = event.keyCode - KeyCode.DIGIT_1;
+        const ref = refs[index];
+        if (ref && ref.current && ref.current.value) {
+          const value = ref.current.value();
+          copyToClipboard(value);
+          setAlertText(
+            <>
+              Copied to clipboard
+              {' '}
+              <code>{value}</code>
+            </>
+          );
+        }
+      }
+      if ((KeyCode.A <= event.keyCode && event.keyCode <= KeyCode.Z)) {
+        refreshTimestamps();
+      }
     };
     windowOnKeyDownListener.current = listener;
     window.addEventListener('keydown', listener);
   }
+
+  
+  const alertBox = alertText 
+    ? <Alert color="success">{alertText}</Alert>
+    : null;
 
   return (
     <div>
@@ -92,16 +161,17 @@ function App() {
         <MomentContext.Provider value={mmt}>
           <Navbar />
           <Container className="pt-3">
+            {alertBox}
             <Button onClick={refreshTimestamps} className="float-right">Refresh</Button>
             <h1><code>Frequently Used</code></h1>
-            <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
-            <Moment fmt="YYYY/MM/DD [W]W/[D]E dddd" />
-            <Moment fmt="LTS: " />
+            <Moment ref={refs[KeyboardToIndex.KEY_1]} fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
+            <Moment ref={refs[KeyboardToIndex.KEY_2]} fmt="YYYY/MM/DD [W]W/[D]E dddd" />
+            <Moment ref={refs[KeyboardToIndex.KEY_3]} fmt="LTS: " />
             <h1><code>Date</code></h1>
             <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a>
-            <Field code="Math.floor(Date.now() / 1000)" content={Math.floor(Date.now() / 1000).toString()} />
-            <Field code="Date.now()" content={Date.now().toString()} />
-            <Field code="new Date().toISOString()" content={new Date().toISOString()} />
+            <Field code="Math.floor(Date.now() / 1000)" content={Math.floor(date.getTime() / 1000).toString()} />
+            <Field code="Date.now()" content={date.getTime().toString()} />
+            <Field code="new Date().toISOString()" content={date.toISOString()} />
             <h1><code>strftime</code></h1>
             <a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a>
             <Time fmt="%H:%M%P" />
@@ -111,6 +181,21 @@ function App() {
             <a href="http://momentjs.com/">http://momentjs.com/</a>
             <Moment fmt="dddd Do MMMM YYYY LTS" />
             <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
+            Keyboard Shortcuts:
+            <ul>
+              <li>
+Press any key from
+                <code>A to Z</code>
+                {' '}
+to refresh. Case insensitive.
+              </li>
+              <li>
+Press any key from
+                <code>1 to 0</code>
+                {' '}
+to copy to clipboard.
+              </li>
+            </ul>
             Packages:
             <ul>
               <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a></li>
