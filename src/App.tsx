a257a3d9ceacbe79f857b2d3f66d55fd926be2b8
Aizat Faiz <aizat.faiz@gmail.com>
2019-03-19 16:41:20 +0800

    fix: create SettingsContext

--- src/App.tsx
+++ src/App.tsx
@@ -43,10 +43,14 @@ const history = {
   ]
 };
 
-const DateContext = React.createContext<Date>(history.dates[0]);
-const MomentContext = React.createContext<moment.Moment>(history.moments[0]);
+const SettingsContext = React.createContext({
+  date: history.dates[0],
+  moment: history.moments[0],
+  setClipboard: (_: string) => {},
+});
 
 const Field = React.forwardRef((props: {content: string, code: string}, ref) => {
+  const { setClipboard } = useContext(SettingsContext);
   if (ref) {
     useImperativeHandle(
       ref,
@@ -63,7 +67,7 @@ const Field = React.forwardRef((props: {content: string, code: string}, ref) => 
       <code>{props.code}</code>
       <InputGroup>
         <InputGroupAddon addonType="prepend">
-          <Button onClick={() => copyToClipboard(props.content)}>Copy</Button>
+          <Button onClick={() => setClipboard(props.content)}>Copy</Button>
         </InputGroupAddon>
         <Input value={props.content} readOnly />
       </InputGroup>
@@ -72,13 +76,13 @@ const Field = React.forwardRef((props: {content: string, code: string}, ref) => 
 });
 
 const Time = React.forwardRef((props: {fmt: string}, ref) => {
-  const date = useContext(DateContext);
+  const { date } = useContext(SettingsContext);
   const str = strftime(props.fmt, date);
   return <Field ref={ref} code={`strftime("${props.fmt}")`} content={str} />;
 });
 
 const Moment = React.forwardRef((props: {fmt: string }, ref) => {
-  const mmt = useContext(MomentContext);
+  const { moment: mmt } = useContext(SettingsContext);
   const str = mmt.format(props.fmt);
   return <Field ref={ref} code={`moment().format("${props.fmt}")`} content={str} />;
 });
@@ -89,6 +93,10 @@ function App() {
   const [mmt, setMoment] = useState(history.moments[0]);
   const [alertText, setAlertText] = useState<JSX.Element | null>();
   const windowOnKeyDownListener = useRef<(event: KeyboardEvent) => void>();
+
+  const settings = useContext(SettingsContext);
+  settings.date = date;
+  settings.moment = mmt;
 
   // I don't use a loop because it is a Hook Rule to not place hooks
   // in loops
@@ -116,20 +124,24 @@ function App() {
   }
 
   if (!windowOnKeyDownListener.current) {
+    settings.setClipboard = (value) => {
+      copyToClipboard(value);
+      setAlertText(
+        <>
+          Copied to clipboard:
+          {' '}
+          <code>{value}</code>
+        </>
+      );
+    }
+
     const listener = (event: KeyboardEvent) => {
       if ((KeyCode.DIGIT_1 <= event.keyCode && event.keyCode <= KeyCode.DIGIT_9)) {
         const index = event.keyCode - KeyCode.DIGIT_1;
         const ref = refs[index];
         if (ref && ref.current && ref.current.value) {
           const value = ref.current.value();
-          copyToClipboard(value);
-          setAlertText(
-            <>
-              Copied to clipboard
-              {' '}
-              <code>{value}</code>
-            </>
-          );
+          settings.setClipboard(value);
         }
       }
       if ((KeyCode.A <= event.keyCode && event.keyCode <= KeyCode.Z)) {
@@ -139,7 +151,6 @@ function App() {
     windowOnKeyDownListener.current = listener;
     window.addEventListener('keydown', listener);
   }
-
   
   const alertBox = alertText 
     ? <Alert color="success">{alertText}</Alert>
@@ -147,55 +158,55 @@ function App() {
 
   return (
     <div>
-      <DateContext.Provider value={date}>
-        <MomentContext.Provider value={mmt}>
-          <Navbar />
-          <Container className="pt-3">
-            {alertBox}
-            <Button onClick={refreshTimestamps} className="float-right">Refresh</Button>
-            <h1><code>Frequently Used</code></h1>
-            <Moment ref={refs[KeyboardToIndex.KEY_1]} fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
-            <Moment ref={refs[KeyboardToIndex.KEY_2]} fmt="YYYY/MM/DD [W]W/[D]E dddd" />
-            <Moment ref={refs[KeyboardToIndex.KEY_3]} fmt="LTS: " />
-            <h1><code>Date</code></h1>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a>
-            <Field code="Math.floor(Date.now() / 1000)" content={Math.floor(date.getTime() / 1000).toString()} />
-            <Field code="Date.now()" content={date.getTime().toString()} />
-            <Field code="new Date().toISOString()" content={date.toISOString()} />
-            <h1><code>strftime</code></h1>
-            <a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a>
-            <Time fmt="%H:%M%P" />
-            <Time fmt="%Y/%m/%d" />
-            <Time fmt="%Y/%m/%d %H:%M:%S%P" />
-            <h1><code>moment</code></h1>
-            <a href="http://momentjs.com/">http://momentjs.com/</a>
-            <Moment fmt="dddd Do MMMM YYYY LTS" />
-            <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
-            Keyboard Shortcuts:
-            <ul>
-              <li>
-Press any key from
-                <code>A to Z</code>
-                {' '}
-to refresh. Case insensitive.
-              </li>
-              <li>
-Press any key from
-                <code>1 to 0</code>
-                {' '}
-to copy to clipboard.
-              </li>
-            </ul>
-            Packages:
-            <ul>
-              <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a></li>
-              <li><a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a></li>
-              <li><a href="http://momentjs.com/">http://momentjs.com/</a></li>
-              <li><a href="https://date-fns.org/">https://date-fns.org/</a></li>
-            </ul>
-          </Container>
-        </MomentContext.Provider>
-      </DateContext.Provider>
+      <SettingsContext.Provider value={settings}>
+        <Navbar />
+        <Container className="pt-3">
+          {alertBox}
+          <Button onClick={refreshTimestamps} className="float-right">Refresh</Button>
+          <h1><code>Frequently Used</code></h1>
+          <Moment ref={refs[KeyboardToIndex.KEY_1]} fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
+          <Moment ref={refs[KeyboardToIndex.KEY_2]} fmt="YYYY/MM/DD [W]W/[D]E dddd" />
+          <Moment ref={refs[KeyboardToIndex.KEY_3]} fmt="LTS: " />
+          <h1><code>Date</code></h1>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a>
+          <Field code="Math.floor(Date.now() / 1000)" content={Math.floor(date.getTime() / 1000).toString()} />
+          <Field code="Date.now()" content={date.getTime().toString()} />
+          <Field code="new Date().toISOString()" content={date.toISOString()} />
+          <h1><code>strftime</code></h1>
+          <a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a>
+          <Time fmt="%H:%M%P" />
+          <Time fmt="%Y/%m/%d" />
+          <Time fmt="%Y/%m/%d %H:%M:%S%P" />
+          <h1><code>moment</code></h1>
+          <a href="http://momentjs.com/">http://momentjs.com/</a>
+          <Moment fmt="dddd Do MMMM YYYY LTS" />
+          <Moment fmt="YYYY/MM/DD LTS - [W]W/[D]E dddd" />
+          Keyboard Shortcuts:
+          <ul>
+            <li>
+              Press any key from
+              {' '}
+              <code>A to Z</code>
+              {' '}
+              to refresh. Case insensitive.
+            </li>
+            <li>
+              Press any key from
+              {' '}
+              <code>1 to 0</code>
+              {' '}
+              to copy to clipboard.
+            </li>
+          </ul>
+          Packages:
+          <ul>
+            <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date</a></li>
+            <li><a href="https://www.npmjs.com/package/strftime">https://www.npmjs.com/package/strftime</a></li>
+            <li><a href="http://momentjs.com/">http://momentjs.com/</a></li>
+            <li><a href="https://date-fns.org/">https://date-fns.org/</a></li>
+          </ul>
+        </Container>
+      </SettingsContext.Provider>
     </div>
   );
 }
